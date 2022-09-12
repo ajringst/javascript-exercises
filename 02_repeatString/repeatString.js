4a0b560b84162c50adf8a9fff283024d11feda20
ajringst <aj.ringstad@gmail.com>
2022-09-12 16:59:42 -0600

    add if and else for negative num scenario

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -3,10 +3,15 @@ let word = '';
 const repeatString = function(string, num) {
     word = string;
     let finalPhrase = '';
-    for (i = 0 ; i < num ; i++){
-        finalPhrase += word;
+    if (num < 0){
+        return 'ERROR';
     }
-    return finalPhrase;
+    else {
+        for (i = 0 ; i < num ; i++){
+            finalPhrase += word;
+        }
+        return finalPhrase;
+    }
 };
 
 // Do not edit below this line
