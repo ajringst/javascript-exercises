df435154568c20e1c5151a7685c3eb0839540622
ajringst <aj.ringstad@gmail.com>
2022-10-07 14:44:29 -0500

    complete reverseString

--- 03_reverseString/README.md
+++ 03_reverseString/README.md
@@ -1,3 +1,4 @@
+
 # Exercise 03 - Reverse a String
 
 Pretty simple, write a function called `reverseString` that returns its input, reversed!
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let text = string;
+    let textArray = text.split("");
+    let textArrayReversed = textArray.reverse();
+    let reversedText = textArrayReversed.join("");
+    return reversedText;
 };
 
 // Do not edit below this line
