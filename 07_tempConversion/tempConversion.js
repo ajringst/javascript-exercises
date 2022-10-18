c09c343d6ac1d135262a17c83938759fe368994a
ajringst <aj.ringstad@gmail.com>
2022-10-18 12:42:07 -0500

    solve tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+let ftemp = temp;
+let ctemp = Math.round(((ftemp - 32) * 5 / 9) * 10) / 10;
+return ctemp;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+let ctemp = temp;
+let ftemp = Math.round(((ctemp * 9 / 5) +32) * 10) / 10;
+return ftemp;
 };
 
 // Do not edit below this line
