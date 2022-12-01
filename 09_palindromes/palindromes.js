a696c98adeb97aa34317099e0cbe05aa7b8e620e
ajringst <aj.ringstad@gmail.com>
2022-11-30 20:13:06 -0700

    solve exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
-};
+const palindromes = function (word) {
+    let onlyAlpha = word.replace(/\W/g, '');
+    let allLower = onlyAlpha.toLowerCase();
+    let length = allLower.length;
+    let i = 0;
+    while(i < length){
+        if(allLower.charAt(i) != allLower.charAt((length - 1 -i))) {
+            return false;
+            break;
+        };
+        i += 1;
+        };
+    return true;
+    };
 
 // Do not edit below this line
 module.exports = palindromes;
